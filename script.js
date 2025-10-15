ddaebf520788290bd78d9bc4c4d4e65347e8bffe
cobiememe <dokanpro@proton.me>
2025-10-15 18:03:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Sound laden
 const hoverSound = new Audio('sounds/hover.mp3');
-hoverSound.volume = 0.2;
+hoverSound.volume = 0.3;
 
 // Hover-Effekte für alle .fx Bilder (Trump Bilder)
 document.querySelectorAll('.fx').forEach(img => {
@@ -37,4 +37,5 @@ if (cobieImg) {
     cobieImg.src = "img/cobie.jpg";
   });
 }
+
 
